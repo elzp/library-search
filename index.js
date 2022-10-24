40ca2e274e69c963acdb5f6f243c95d62dfcb74e
elzp <86740659+elzp@users.noreply.github.com>
2022-10-24 21:26:06 +0200

    Add listener to input

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import './style.css';
 
-const API_URL =
-  'https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/auto_complete?format=json&q=harry%20potter';
+const input = document.querySelector('[id=book-name]');
+console.log(input);
+let inputedText;
+input.addEventListener('change', (e) => {
+  inputedText = e.target.value;
+});
 
-var requestURL =
-  'https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/auto_complete?format=json&q=riordan';
+
+var requestURL = `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/auto_complete?format=json&q=${inputedText}`;
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
