ab9fb46f60b7a31490247cb75eb68f67706c7ca5
elzp <86740659+elzp@users.noreply.github.com>
2022-10-24 21:37:38 +0200

    Make function for requesting data

--- index.js
+++ index.js
@@ -13,14 +13,17 @@ button.addEventListener('click', (e) => {
   e.preventDefault();
 });
 
+function request(textToFind) {
+  var requestURL = `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/auto_complete?format=json&q=${textToFind}`;
+  var request = new XMLHttpRequest();
+  request.open('GET', requestURL);
+  request.responseType = 'json';
+  request.send();
 
-var requestURL = `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/book/auto_complete?format=json&q=${inputedText}`;
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-
-request.onload = function () {
-  var response = request.response;
-  console.log(response);
-};
+  request.onload = function () {
+    resultOfRequest = request.response.map((it) => {
+      return { title: it.title, image: it.imageUrl };
+    });
+    console.log(resultOfRequest);
+  };
+}
