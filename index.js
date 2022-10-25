22a570fdfbc2f9481eae09c08a3daeff8dc8922c
elzp <86740659+elzp@users.noreply.github.com>
2022-10-25 19:19:32 +0200

    Add injection of search results to html

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 import './style.css';
 
 const input = document.querySelector('[id=book-name]');
-console.log(input);
+
 let inputedText;
 input.addEventListener('change', (e) => {
   inputedText = e.target.value;
 });
-
 const button = document.querySelector('[id=form__button]');
 const resultsContainer = document.querySelector('.results');
+resultsContainer.innerHTML='';
+let resultOfRequest;
 
-button.addEventListener('click', (e) => {
+button.addEventListener('click', async (e) => {
   e.preventDefault();
+  await request(inputedText);
   resultsContainer.innerHTML='';
-
+  let htmlToInject = resultOfRequest.map(it=>makeBookEl(it)).join('')
+  resultsContainer.innerHTML = htmlToInject;
 });
 
 function request(textToFind) {
@@ -27,7 +30,6 @@ function request(textToFind) {
     resultOfRequest = request.response.map((it) => {
       return { title: it.title, image: it.imageUrl };
     });
-    console.log(resultOfRequest);
   };
 }
 
